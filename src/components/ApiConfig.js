6f0cfa76105a64fe924024fe48fc9e28417fa68e
kartikpande123 <kartikpande67@gmail.com>
2025-10-24 09:34:47 +0530

    fix bug 01

--- src/components/ApiConfig.js
+++ src/components/ApiConfig.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://arnprivateexamconduct.in";
-const API_BASE_URL = "http://localhost:2025";
+const API_BASE_URL = "https://arnprivateexamconduct.in";
+// const API_BASE_URL = "http://localhost:2025";
 export default API_BASE_URL;
 
 
